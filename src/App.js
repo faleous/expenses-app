65fa2adc494b82236287302e0f28c8e619665e15
Faleous <myfirsttwink1@gmail.com>
2022-07-17 17:27:34 +0300

    cost items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import CostItem from './components/CostItem';
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2022, 4, 10),
+      description: 'Financial operations',
+      amount: 12.33
+    },
+    {
+      date: new Date(2022, 4, 11),
+      description: 'Food',
+      amount: 2.33
+    }
+  ]
+
   return (
     <div className="App">
       Let's get started!
+      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
+      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
     </div>
   );
 }
---
+++ src/components/CostItem.js
@@ -0,0 +1,13 @@
+import "./CostItemStyles.css";
+
+export default function CostItem(props) {
+    return(
+        <div className="cost-item-wrapper">
+            <p className='cost-item-date'> {props.date.toDateString()} </p>
+            <div className='price-description-wrapper'>
+                <p>{props.description}</p>
+                <p className='cost-item-price'>{props.amount}</p>
+            </div>
+        </div>
+    )
+}
